634d557fe68b9827adc7bbec54e94932394d8b91
Daniel Walther <DWalther@anatomy.uzh.ch>
2023-08-14 15:56:28 +0200

    generic commit message, Mon 15:56:27 14.08.2023.

--- readTifFormatTest.py
+++ readTifFormatTest.py
@@ -3,7 +3,7 @@ import os
 import fileHandling as fH
 
 
-def main(file_paths=["M:/data/d.walther/Microscopy/babb03/tiff-ct3/-crop-bicubic-scaled0.25-autofluo-hyperstackSequence/id01-Ch405,488,561nm-crop-scaled0.25-hyperstackSequence.tif", "M:/data/d.walther/Microscopy/babb03/tiff-ct3/-crop-bicubic-scaled0.25-autofluo-hyperstackRGB24/id01-Ch405,488,561nm-crop-scaled0.25-hyperstackRGB.tif", "M:/data/d.walther/Microscopy/babb03/tiff-ct3/-crop-bicubic-scaled0.25-autofluo-hyperstackRGB24-czyx/id01-Ch405,488,561nm-crop-scaled0.25-hyperstackRGB-czyx.tif"]):
+def main(file_paths=["M:/data/d.walther/Microscopy/babb03/tiff-ct3/dataset02/-crop-bicubic-scaled0.25-autofluo-hyperstackSequence/id01-Ch405,488,561nm-crop-scaled0.25-hyperstackSequence.tif", "M:/data/d.walther/Microscopy/babb03/tiff-ct3/-crop-bicubic-scaled0.25-autofluo-hyperstackRGB24/id01-Ch405,488,561nm-crop-scaled0.25-hyperstackRGB.tif", "M:/data/d.walther/Microscopy/babb03/tiff-ct3/-crop-bicubic-scaled0.25-autofluo-hyperstackRGB24-czyx/id01-Ch405,488,561nm-crop-scaled0.25-hyperstackRGB-czyx.tif"]):
 
     for file in file_paths:
 
@@ -19,20 +19,28 @@ def main(file_paths=["M:/data/d.walther/Microscopy/babb03/tiff-ct3/-crop-bicubic
 
 if __name__ == "__main__":
 
-    file_paths = ["M:/data/d.walther/Microscopy/babb03/tiff-ct3/-crop-bicubic-scaled0.25-autofluo-hyperstackSequence/id01-Ch405,488,561nm-crop-scaled0.25-hyperstackSequence.tif",
-             "M:/data/d.walther/Microscopy/babb03/tiff-ct3/-crop-bicubic-scaled0.25-autofluo-hyperstackRGB24/id01-Ch405,488,561nm-crop-scaled0.25-hyperstackRGB.tif",
-             "M:/data/d.walther/Microscopy/babb03/tiff-ct3/-crop-bicubic-scaled0.25-autofluo-hyperstackRGB24-czyx/id01-Ch405,488,561nm-crop-scaled0.25-hyperstackRGB-czyx.tif"]  # this line: my reformatted one, should be Czyx (3, z, y, x)
+    file_paths = ["M:/data/d.walther/Microscopy/babb03/tiff-ct3/dataset02/-crop-bicubic-scaled0.25-autofluo-hyperstackSequence/id01-Ch405,488,561nm-crop-scaled0.25-hyperstackSequence.tif",
+             "M:/data/d.walther/Microscopy/babb03/tiff-ct3/dataset02/-crop-bicubic-scaled0.25-autofluo-hyperstackRGB24/id01-Ch405,488,561nm-crop-scaled0.25-hyperstackRGB.tif",
+             "M:/data/d.walther/Microscopy/babb03/tiff-ct3/dataset02/-crop-bicubic-scaled0.25-autofluo-hyperstackRGB24-czyx/id01-Ch405,488,561nm-crop-scaled0.25-hyperstackRGB-czyx.tif"]  # this line: my reformatted one, should be Czyx (3, z, y, x)
 
-    # folder 1
-    path = fH.get_directory_dialog()
-    files = fH.get_file_list(path)
-    file_paths = [path + file for file in files]
-    main(file_paths)
+    static = True
 
-    # folder 2
-    path = fH.get_directory_dialog()
-    files = fH.get_file_list(path)
-    file_paths = [path + file for file in files]
-    main(file_paths)
+    if static:
+
+        main(file_paths)
+
+    else:
+        
+        # folder 1
+        path = fH.get_directory_dialog()
+        files = fH.get_file_list(path)
+        file_paths = [path + file for file in files]
+        main(file_paths)
+
+        # folder 2
+        path = fH.get_directory_dialog()
+        files = fH.get_file_list(path)
+        file_paths = [path + file for file in files]
+        main(file_paths)
 
     exit(0)
