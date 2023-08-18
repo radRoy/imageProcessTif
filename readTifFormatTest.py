a3620d45ff3aff18506f6ccd2e09020f509fb3c2
radRoy <daniel.walther@uzh.ch>
2023-08-18 16:30:18 +0200

    Copy the readTifFormatTest.py output for 4 representative formatting tif files into comments in that .py scripts (at the very end)

--- readTifFormatTest.py
+++ readTifFormatTest.py
@@ -19,9 +19,17 @@ def main(file_paths=["M:/data/d.walther/Microscopy/babb03/tiff-ct3/dataset02/-cr
 
 if __name__ == "__main__":
 
-    file_paths = ["M:/data/d.walther/Microscopy/babb03/tiff-ct3/dataset02/-crop-bicubic-scaled0.25-autofluo-hyperstackSequence/id01-Ch405,488,561nm-crop-scaled0.25-hyperstackSequence.tif",
-             "M:/data/d.walther/Microscopy/babb03/tiff-ct3/dataset02/-crop-bicubic-scaled0.25-autofluo-hyperstackRGB24/id01-Ch405,488,561nm-crop-scaled0.25-hyperstackRGB.tif",
-             "M:/data/d.walther/Microscopy/babb03/tiff-ct3/dataset02/-crop-bicubic-scaled0.25-autofluo-hyperstackRGB24-czyx/id01-Ch405,488,561nm-crop-scaled0.25-hyperstackRGB-czyx.tif"]  # this line: my reformatted one, should be Czyx (3, z, y, x)
+    file_paths = [
+        "M:/data/d.walther/Microscopy/babb03/tiff-ct3/dataset02/-crop-bicubic-scaled0.25-autofluo-hyperstackSequence/id01-Ch405,488,561nm-crop-scaled0.25-hyperstackSequence.tif",
+        "M:/data/d.walther/Microscopy/babb03/tiff-ct3/dataset02/-crop-bicubic-scaled0.25-autofluo-hyperstackRGB24/id01-Ch405,488,561nm-crop-scaled0.25-hyperstackRGB.tif",
+        "M:/data/d.walther/Microscopy/babb03/tiff-ct3/dataset02/-crop-bicubic-scaled0.25-autofluo-hyperstackRGB24-czyx/id01-Ch405,488,561nm-crop-scaled0.25-hyperstackRGB-czyx.tif"]  # this line: my reformatted one, should be Czyx (3, z, y, x)
+        # see output comment section at the end for output of these file_paths
+
+
+    file_paths = [
+        'M:/data/d.walther/Microscopy/babb03/tiff-ct3/dataset02/-crop-bicubic-scaled0.25-autofluo/id01-Ch405nm-crop-scaled0.25.tif'
+    ]
+        # see output comment section at the end for output of these file_paths
 
     static = True
 
@@ -42,5 +50,21 @@ if __name__ == "__main__":
         files = fH.get_file_list(path)
         file_paths = [path + file for file in files]
         main(file_paths)
-
+    
     exit(0)
+
+# some outputs:
+'''
+C:/Users/Dancer/Documents/imageProcessTif/venv/Scripts/python.exe C:/Users/Dancer/Documents/imageProcessTif/readTifFormatTest.py 
+shape: (327, 1102, 371) , image filepath M:/data/d.walther/Microscopy/babb03/tiff-ct3/dataset02/-crop-bicubic-scaled0.25-autofluo-hyperstackSequence/id01-Ch405,488,561nm-crop-scaled0.25-hyperstackSequence.tif
+shape: (109, 1102, 371, 3) , image filepath M:/data/d.walther/Microscopy/babb03/tiff-ct3/dataset02/-crop-bicubic-scaled0.25-autofluo-hyperstackRGB24/id01-Ch405,488,561nm-crop-scaled0.25-hyperstackRGB.tif
+shape: (3, 109, 1102, 371) , image filepath M:/data/d.walther/Microscopy/babb03/tiff-ct3/dataset02/-crop-bicubic-scaled0.25-autofluo-hyperstackRGB24-czyx/id01-Ch405,488,561nm-crop-scaled0.25-hyperstackRGB-czyx.tif
+
+Process finished with exit code 0
+'''
+'''
+C:/Users/Dancer/Documents/imageProcessTif/venv/Scripts/python.exe C:/Users/Dancer/Documents/imageProcessTif/readTifFormatTest.py 
+shape: (109, 1102, 371) , image filepath M:/data/d.walther/Microscopy/babb03/tiff-ct3/dataset02/-crop-bicubic-scaled0.25-autofluo/id01-Ch405nm-crop-scaled0.25.tif
+
+Process finished with exit code 0
+'''
