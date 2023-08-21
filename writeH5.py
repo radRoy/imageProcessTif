c291aa3ddf96077e20058b1979721d44ab05599a
radRoy <daniel.walther@uzh.ch>
2023-08-21 15:54:12 +0200

    Refine and revise writeH5.py - add some assertion tests, some print statements, etc.

--- writeH5.py
+++ writeH5.py
@@ -1,8 +1,7 @@
 import os.path
-
+import tkinter as tk
 import fileHandling as fH
 import h5py
-import numpy as np
 import readH5
 
 
@@ -31,6 +30,12 @@ def tif_append_h5(data_array, h5_path, internal_path):
 
 if __name__ == "__main__":
 
+    # This puts the tkinter dialog window (for choosing inputs etc.) on top of other windows.
+    window = tk.Tk()
+    window.wm_attributes('-topmost', 1)
+    window.withdraw()  # this suppresses the tk window
+
+    ''' STATIC PARAMETERS '''
     # Caution: Do not forget to check the 'internal_path=...' argument below when calling the h5 append/create function!
     # set the mode for creating new or appending to HDF5 file(s)
     label_path = "label"
@@ -46,33 +51,43 @@ if __name__ == "__main__":
         1. input directory
     output directory will be created automatically in this case
     """
+    ''' STATIC PARAMETERS END '''
+
+    ''' INPUT FILE PATHS '''
 
     file_paths = []  # the tif input files. to be saved in hdf5 format
     h5_file_paths = []  # the h5 files to be created or appended to.
 
-    # INPUT FILE PATHS
     path = fH.get_folder_path_dialog()  # str: path with slashes and trailing slash
     files = fH.get_file_list(path)  # list: of the filenames (with extension) contained in the given path
     # tif file paths - assume these are formatted correctly (czyx)
     file_paths = [path + file for file in files]
 
+    ''' INPUT FILE PATHS - H5 APPEND CASE '''
+
     if mode_append:
         # OUTPUT FILE PATHS, directories, etc.
-        path_out = fH.get_folder_path_dialog()  # str with trailing slash (path contains h5 files)
-        h5_files = fH.get_file_list(path_out)
-        h5_file_paths = [path_out + file for file in h5_files]
+        path_out_append = fH.get_folder_path_dialog()  # str with trailing slash (path contains h5 files)
+        h5_files = fH.get_file_list(path_out_append)
+        h5_file_paths = [path_out_append + file for file in h5_files]
+
+        #print(f'\nh5 append mode: h5 file paths:')
+        #fH.iterate_function_args_over_iterable(h5_file_paths, print)
+        #exit(0)
+
+    ''' INPUT FILE PATHS - H5 NEW CASE '''
 
     if not mode_append:
         # OUTPUT FILE PATHS, directories, etc.
-        path_out = path.strip("/") + "-h5/"  # str: path with appended -h5
-        created_path = fH.create_sibling_dir(path_out)  # (str output, not required) create output path if it doesn't exist yet
+        suffix = "-h5"  # suffix should be given without trailing slash
+        path_out_new = fH.create_sibling_dir(path=path, suffix=suffix)  # (str output, not required) create output path if it doesn't exist yet
         # print(path_out == created_path)  # testing (prints True)
 
         # file paths of the h5 files to be created / appended to
         h5_file_paths = []
         for i, file in enumerate(files):
             filename_h5 = fH.rename_file(file, suffix="-h5", extension="h5")
-            h5_file_paths.append(path_out + filename_h5)
+            h5_file_paths.append(path_out_new + filename_h5)
             """
             #testing
             print(i, file)
@@ -80,7 +95,34 @@ if __name__ == "__main__":
             print(h5_file_paths[i])
             """
 
-    # creating the h5 files
+        #print(f'\nh5 create mode: h5 file paths:')
+        #fH.iterate_function_args_over_iterable(h5_file_paths, print)
+        #exit(0)
+
+    ''' ENSURING THAT INPUT AND OUTPUT FILE PATHS ARE BOTH SORTED ASCENDINGLY & OF EQUAL LENGTH '''
+
+    # sorting the file path lists to ensure correct specimen correspondence between raw and label input internal paths
+    #print(f'\npre-sorted file paths:')
+    #fileHandling.iterate_function_args_over_iterable(file_paths, print)
+    #print(f'\npre-sorted h5 (output) file paths:')
+    #fileHandling.iterate_function_args_over_iterable(h5_file_paths, print)
+    ##file_paths = sorted(file_paths, reverse=True)  # double-checking that sorted() actually does something & what is expected.
+    ##h5_file_paths = sorted(h5_file_paths, reverse=True)  # double-checking that sorted() actually does something & what is expected.
+    file_paths = sorted(file_paths)  # sorts ascendingly
+    h5_file_paths = sorted(h5_file_paths)  # sorts ascendingly
+    #print(f'\nascendingly sorted file paths:')
+    #fileHandling.iterate_function_args_over_iterable(file_paths, print)
+    #print(f'\nascendingly sorted h5 (output) file paths:')
+    #fileHandling.iterate_function_args_over_iterable(h5_file_paths, print)
+    #exit(0)
+
+    #h5_file_paths = h5_file_paths[1:]  # testing assert statement below
+    assert len(file_paths) == len(h5_file_paths), 'writeH5.py: Input and output file path lists should contain the same number of file paths (but they do not).'
+    #exit(0)
+
+    ''' H5 CREATION / APPENDING, ITERATIVELY '''
+
+    # creating the h5 files (deprecated, handled below with function 'tif_append_h5()')
     """internal_path_raw = "raw"
     for i, file_path in enumerate(file_paths):
         a_tif = fH.read_tif_stack(file_path)
@@ -88,17 +130,23 @@ if __name__ == "__main__":
         created_file_path_out = tif_create_h5(a_tif, h5_file_paths[i], internal_path_raw)
         break  # testing"""
 
-    # appending to the h5 files
+    # appending to the h5 files (creating new or appending to existing ones works the same in ~'append' file saving mode)
     # Caution: Do not forget to check the internal_path variable above (see append_mode ~)
+    if mode_append:
+        print(f'\nH5 files are going to be appended to.')
+    else:
+        print(f'\nNew H5 files are going to be created.')
     for i, file_path in enumerate(file_paths):
+
         a_tif = fH.read_tif_stack(file_path)
-        print(type(a_tif), a_tif.shape, file_path)  # testing
+        print(f'\nA tif file has been opened and will be written to an H5 file. Properties of this tif file:')
+        print(f' python type: {type(a_tif)}\n python shape (format): {a_tif.shape}\n file path: {file_path}')  # testing
         created_file_path_out = tif_append_h5(data_array=a_tif, h5_path=h5_file_paths[i], internal_path=internal_path)
         # break  # testing
 
         # testing (verification of shape in created h5 file)
         h5file = readH5.open_h5(created_file_path_out)
         for key in list(h5file.keys()):
-            print(f"shape of py-created h5, 'key' {key}: {h5file[key].shape}")
+            print(f"Shape of python-saved H5 file / internal H5 path: 'key' {key}: {h5file[key].shape}")
 
-        print("")  # added readability (add a newline between data sets)
+    exit(0)
