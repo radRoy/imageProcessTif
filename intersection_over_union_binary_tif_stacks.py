81d8ec901b7335705e2a4126710803b5ffb5c7d7
Daniel Walther <DWalther@anatomy.uzh.ch>
2024-02-15 11:59:35 +0100

    generic commit message, Thu 11:59:35 15.02.2024.

--- intersection_over_union_binary_tif_stacks.py
+++ intersection_over_union_binary_tif_stacks.py
@@ -1,7 +1,8 @@
 import skimage
-import 
+import cv2
 import os
-import imageProcessTif.fileHandling as fH
+import matplotlib.pyplot as plt
+import fileHandling as fH
 
 
 def main(file_paths=["M:/data/d.walther/Microscopy/babb03/tiff-ct3/dataset02/-crop-bicubic-scaled0.25-autofluo-hyperstackSequence/id01-Ch405,488,561nm-crop-scaled0.25-hyperstackSequence.tif", "M:/data/d.walther/Microscopy/babb03/tiff-ct3/-crop-bicubic-scaled0.25-autofluo-hyperstackRGB24/id01-Ch405,488,561nm-crop-scaled0.25-hyperstackRGB.tif", "M:/data/d.walther/Microscopy/babb03/tiff-ct3/-crop-bicubic-scaled0.25-autofluo-hyperstackRGB24-czyx/id01-Ch405,488,561nm-crop-scaled0.25-hyperstackRGB-czyx.tif"]):
@@ -16,3 +17,30 @@ def main(file_paths=["M:/data/d.walther/Microscopy/babb03/tiff-ct3/dataset02/-cr
             print("\nFile", file, "does not exist.")
 
     return 0
+
+
+if __name__ == "__main__":
+
+    print("")
+
+    #import cv2
+
+    img1 = cv2.imread('black_top_right_triangle.png', 0)
+    img2 = cv2.imread('black_bottom_right_triangle.png', 0)
+
+    print(type(img1))
+    print(img1)
+
+    img_bwa = cv2.bitwise_and(img1, img2)
+    img_bwo = cv2.bitwise_or(img1, img2)
+    img_bwx = cv2.bitwise_xor(img1, img2)
+
+    print(type(img_bwa))
+    print(img_bwa)
+    #plt.imshow(img_bwa)
+
+    #cv2.imshow("Bitwise AND of Image 1 and 2", img_bwa)
+    #cv2.imshow("Bitwise OR of Image 1 and 2", img_bwo)
+    #cv2.imshow("Bitwise XOR of Image 1 and 2", img_bwx)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
